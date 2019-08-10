d38dc9ca3734640fdbdd4266819e74594dfd9a01
LeeYongwon <dyddnjs0982@naver.cpm>
2019-08-10 17:56:17 +0900

    기본적인 끝말잇기 완성

--- React/wordchain/WordRelay.jsx
+++ React/wordchain/WordRelay.jsx
@@ -1,18 +1,41 @@
 const React = require('react');
-const ReactDom=require('react-dom');
-const { Component} =React;
+const ReactDom = require('react-dom');
+const { Component, useState, useRef } = React;
 
-class WordRelay extends Component{
-    state={
-        value:'ㅎㅇㅎㅎㅇㅎㅇㅎ'
+const WordRelay = () => {
+    /*
+        제시단어의 끝글자와 입력단어의 첫번째 글자가 같다면
+        입력단어가 제시단어로 return
+        제시단어의 끝글자를 구하는 방법은
+        firstWord[firstWord.length-1]
+    */
+    const [firstWord, setFirstWord] = useState('이용원');
+    const [value,setValue ] =useState('');
+    const [result, setResult] = useState('');
+
+    const onChange = (e) =>{
+        setValue(e.target.value);
     }
-    render(){
-        return(
-            <React.Fragment>
-                <div>{this.state.value}</div>
-            </React.Fragment>
-        );
+
+    const onClick =(e)=>{
+        e.preventDefault();
+        if(firstWord[firstWord.length-1]===value[0]){
+            setFirstWord(value);
+            setValue('');
+            setResult('정답입니다');
+        }else{
+            setValue('');
+            setResult('틀렸습니다');
+        }
     }
+    return <>
+            <h1>제시어 : {firstWord}</h1>
+        <form>
+            <input type="text" value={value} onChange={onChange}></input>
+            <button onClick={onClick}>버튼</button>
+            <h1>{result}</h1>
+        </form>
+    </>
 }
 
 module.exports = WordRelay;
--- React/wordchain/webpack.config.js
+++ React/wordchain/webpack.config.js
@@ -1,29 +1,38 @@
 const path = require('path');
+const webpack =require('webpack');
 module.exports = {
     name: 'wordchain-settings',//그냥 이름
     mode: 'development',//개발용인지 실서비스를 할때는 production으로 바꿔준다.
     devtool: 'eval',//이건 그냥 빠르게 하겠다라는 뜻이다.
-    resolve:{
-        extensions:['.jsx','.js']//뒤에 확장자를 생략하게 해준다.
+    resolve: {
+        extensions: ['.jsx', '.js']//뒤에 확장자를 생략하게 해준다.
     },
 
     entry: {
-        app:['./client']//어떤 파일을 합쳐서 생성할 것인가.
+        app: ['./client']//어떤 파일을 합쳐서 생성할 것인가.
     },//입력
 
-    module:{//위에 entry에 있는 파일을 읽어서 거기에 module을 적용한 후 output에 빼주는 작업을 한다.
-        rules:[{//적용할 규칙
-            test:/\.jsx?/, //규칙을 적용할 파일들 정규표현식도 공부하자.
-            loader:'babel-loader',//어떤것을 적용할것인가  js와jsx파일에 babel을 적용해서 옛날 브라우저에서도 돌아가게 해주겠다
-            options:{//여기는 적용할 babel의 옵션들을 설정해준다.
-                presets:['@babel/preset-env', '@babel/preset-react'],
-                plugins:['@babel/plugin-proposal-class-properties']
+    module: {//위에 entry에 있는 파일을 읽어서 거기에 module을 적용한 후 output에 빼주는 작업을 한다.
+        rules: [{//적용할 규칙
+            test: /\.jsx?/, //규칙을 적용할 파일들 정규표현식도 공부하자.
+            loader: 'babel-loader',//어떤것을 적용할것인가  js와jsx파일에 babel을 적용해서 옛날 브라우저에서도 돌아가게 해주겠다
+            options: {//여기는 적용할 babel의 옵션들을 설정해준다.
+                presets: [
+                    ['@babel/preset-env', {
+                        targets:{
+                            browsers:['> 5% in KR'], 
+                    },
+                    debug: true,
+                }],
+                    '@babel/preset-react'
+                ],
+                plugins: ['@babel/plugin-proposal-class-properties']
             }
         }]
     },
-
+    plugins:[],
     output: {
-        path:path.join(__dirname, 'dist'),//dist라는 폴더에 생성될 파일을 만들어준다.
-        filename:'App.js'//합쳐서 생성할 파일의 이름
+        path: path.join(__dirname, 'dist'),//dist라는 폴더에 생성될 파일을 만들어준다.
+        filename: 'App.js'//합쳐서 생성할 파일의 이름
     }//출력
 }
