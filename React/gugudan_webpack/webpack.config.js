cbf34d356b111c53bac91b71bb188d525e96b09d
LeeYongwon <dyddnjs0982@naver.cpm>
2019-08-10 16:57:21 +0900

    gitignore추가

--- React/gugudan_webpack/webpack.config.js
+++ React/gugudan_webpack/webpack.config.js
@@ -1,5 +1,6 @@
 //여기에 webpack 설정
 const path = require('path');
+const webpack =require('webpack');
 module.exports={
     name:'gugudan',//프로젝트 이름
     mode:'development',
@@ -18,11 +19,23 @@ module.exports={
             test:/\.jsx?/,
             loader:'babel-loader',
             options:{
-                presets:['@babel/preset-env' ,'@babel/preset-react'],
+                presets:[
+                    ['@babel/preset-env', {
+                        targets:{
+                            browsers:['> 5% in KR', 'last 2 chrome versions'],
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react',
+                ]
+                ,
                 plugins:['@babel/plugin-proposal-class-properties']
             }
         }]
     },
+    plugins:[
+        new webpack.LoaderOptionsPlugin({debug: true}),
+    ],
     output:{
         path:path.join(__dirname,'dist'),//어디 경로로 할지
         filename:'App.js'
